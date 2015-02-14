d117edacfa65ce1c00c356cfdc38a735b65c4690
Maria Miller <mariajmiller13@gmail.com>
2015-02-14 14:41:39 -0500

    Get tweets and store in database

--- getTweets.py
+++ getTweets.py
@@ -1,32 +1,53 @@
+
+'''
+Maria Miller - mjm191@zips.uakron.edu
+Idea for this project from:
+http://www.danielforsyth.me/analyzing-a-nhl-playoff-game-with-twitter/
+'''
+
 import tweepy
 import pymongo
 import sys
 
-consumer_key = ""
-consumer_secret = ""
+from pymongo import MongoClient
 
-access_token = ""
-access_token_secret = ""
+# Consumer Keys and access tokens for Twitter here #
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-user = api.me()
-
-print('Name: ' + user.name)
-print('Location: ' + user.location)
-print('Friends: ' + str(user.friends_count))
-
 class customStreamListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
-        super(tweepy.StreamLister, self).__init__()
+        super(tweepy.StreamListener, self).__init__()
 
-        self.db = pymongo.MongoClient.Valentine
+        client = MongoClient()
+        self.db = client.Valentine 
 
-    def store_tweets(self, status):
+    def on_status(self, status):
+        print status.text , "\n"
+
         data = {}
+        data['text'] = status.text
+        data['created_at'] = status.created_at
+        data['geo'] = status.geo
+        data['source'] = status.source
+
+        self.db.Tweets.insert(data)
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', 
+        status_code
+        return True #Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stder, 'Timeout...'
+        return True # Don't kill the stream
+
+sapi = tweepy.streaming.Stream(auth, customStreamListener(api))
+sapi.filter(track=['valentine'])
+
 
 
 
