1b1fc8c9532fed57aa3f9d333381a6b663781dff
Maria Miller <mariajmiller13@gmail.com>
2015-03-26 14:24:33 -0400

    create frequency list.

--- src/wordFreq.py
+++ src/wordFreq.py
@@ -3,16 +3,22 @@
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-from nltk import FreqDist
+import pickle
 
 tweets = pd.read_pickle('../data/valentine.pkl')
 
 textList = tweets["text"]
+
+stop = set(stopwords.words('english'))
 
 tokens = []
 
 for txt in textList:
     tokens.extend([t.lower().strip(";,.") for t in txt.split()])
 
-#filtered_tokens = [w for w in tokens if not w in stopwords.words('english')]
+filtered_tokens = [w for w in tokens if not w in stop]
+
+freq_dist = nltk.FreqDist(filtered_tokens)
+
+
 
