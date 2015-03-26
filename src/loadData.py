032fb2c129b137b44dcd399e6caace7f532988fc
MariaJMiller <MariaJMiller@users.noreply.github.com>
2015-03-26 10:35:05 -0400

    Change file path in loadData.

--- src/loadData.py
+++ src/loadData.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pandas.tseries.offsets import DateOffset
 
 # Import file
-tweets = pd.read_csv('../data/thedress.csv')
+tweets = pd.read_csv('../data/valentine.csv')
 
 # Change index to created_at, change time format to 12 hours.
 tweets['created_at'] = pd.to_datetime(pd.Series(tweets['created_at']))
@@ -14,7 +14,7 @@ tweets.index = tweets.index.tz_localize('GMT').tz_convert('EST')
 tweets.index = tweets.index - DateOffset(hours = 12)
 
 # Save the pandas dataframe.
-tweets.to_pickle('../data/thedress.pkl')
+tweets.to_pickle('../data/valentine.pkl')
 
 
 
