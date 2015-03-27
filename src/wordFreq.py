0a2653162b2a13cf5412f50009376f506e99f338
MariaJMiller <MariaJMiller@users.noreply.github.com>
2015-03-27 13:52:41 -0400

    Get the most frequently tweeted words.

--- src/wordFreq.py
+++ src/wordFreq.py
@@ -3,22 +3,33 @@
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-import pickle
 
+# Get tweets previously saved.
 tweets = pd.read_pickle('../data/valentine.pkl')
 
 textList = tweets["text"]
 
 stop = set(stopwords.words('english'))
 
+# Add stopwords not included in nltk corpus.
+stop.add('&amp')
+stop.add('rt')
+stop.add("it's")
+
 tokens = []
 
+# Remove some punctuation.
 for txt in textList:
     tokens.extend([t.lower().strip(";,.") for t in txt.split()])
 
+# Remove stop words.
 filtered_tokens = [w for w in tokens if not w in stop]
 
+# Create a frequency distribution of words.
 freq_dist = nltk.FreqDist(filtered_tokens)
+
+# Get the top 50 most frequently tweeted words.
+top_words = freq_dist.most_common(50)
 
 
 
