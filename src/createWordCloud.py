0d0cab38fafa0f82ea33077244216eb885219485
Maria Miller <mariajmiller13@gmail.com>
2015-04-03 12:38:48 -0400

    additional images

--- src/createWordCloud.py
+++ src/createWordCloud.py
@@ -9,7 +9,7 @@ from wordcloud import WordCloud
 from scipy.misc import imread
 
 # Get tweets previously saved.
-tweets = pd.read_pickle('../data/single.pkl')
+tweets = pd.read_pickle('../data/valentine.pkl')
 
 # Create single string from tweets.
 words = ' '.join(tweets['text'])
@@ -25,28 +25,29 @@ stop = set(stopwords.words('english'))
 
 # Import image for mask
 # http://www.stencilry.org/stencils/symbols/hearts/heart+1.jpg
-# heart_mask = imread('../images/heart.png', flatten=True)
+heart_mask = imread('../images/heart.png', flatten=True)
 
 # Add stopwords not included in nltk corpus.
 stop.add('&amp')
 stop.add('amp')
 stop.add('rt')
 stop.add("it's")
-stop.add('-')
-stop.add('WhyImSingle')
-# stop.add("valentine's")
+# stop.add('-')
+# stop.add('WhyImSingle')
+stop.add("valentine's")
+stop.add('day')
+stop.add('valentine')
 
 # Create word cloud.
 word_cloud = WordCloud(
   font_path='/Users/MariaJ/Library/Fonts/BD_Cartoon_Shout.ttf',
   stopwords=stop,
-  background_color='black',
-  width=2500,
-  height=2300
+  width=1800,
+  height=1600
   ).generate(no_urls_no_tags)
 plt.imshow(word_cloud)
 plt.axis('off')
-plt.savefig('../images/single.png', dpi=300)
+plt.savefig('../images/val.png', dpi=300)
 plt.show()
 
 
